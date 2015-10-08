2f5297880522042fd65e102f4b850e165372f844
Aviad Hahami <aviadhahami@gmail.com>
2015-10-08 14:59:56 +0300

    Added userrole to enum service

--- public/modules/dashboard/controllers/dashboard.client.controller.js
+++ public/modules/dashboard/controllers/dashboard.client.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('dashboard').controller('dashboardController', ['$scope', 'Authentication',
-    function($scope, Authentication) {
+angular.module('dashboard').controller('dashboardController', ['$scope', 'Authentication','Userroleasenumservice',
+    function($scope, Authentication,Userroleasenumservice) {
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
+        $scope.userLevel = Userroleasenumservice.getValue($scope.user.roles);
+        console.log($scope.userLevel);
         console.log($scope.user);
     }]);
