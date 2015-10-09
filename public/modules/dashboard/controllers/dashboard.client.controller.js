89541ca0f06f1c1aa48080779c63738add98446c
Aviad Hahami <aviadhahami@gmail.com>
2015-10-09 19:09:36 +0300

    Removed loggers & tests

--- public/modules/dashboard/controllers/dashboard.client.controller.js
+++ public/modules/dashboard/controllers/dashboard.client.controller.js
@@ -4,10 +4,7 @@ angular.module('dashboard').controller('dashboardController', ['$scope', 'Authen
     function($scope, Authentication,Userroleasenumservice,Users) {
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
         $scope.userLevel = Userroleasenumservice.getValue($scope.user.roles);
-        console.log($scope.userLevel);
-        $scope.user.roles = 'admin';
-        console.log($scope.user);
-        Users.update($scope.user);
+
 
 
         $scope.links = [
