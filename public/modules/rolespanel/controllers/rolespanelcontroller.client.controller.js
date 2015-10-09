7fa9ff39d6e1328d6b0e8e8019f2808b3501c891
Aviad Hahami <aviadhahami@gmail.com>
2015-10-09 20:38:54 +0300

    Redirects if not an admin

--- public/modules/rolespanel/controllers/rolespanelcontroller.client.controller.js
+++ public/modules/rolespanel/controllers/rolespanelcontroller.client.controller.js
@@ -1,10 +1,14 @@
 'use strict';
 
-angular.module('rolespanel').controller('rolespanelController', ['$scope', 'Authentication','Userroleasenumservice','Users','$http',
-    function($scope, Authentication,Userroleasenumservice,Users, $http) {
+angular.module('rolespanel').controller('rolespanelController', ['$scope', 'Authentication','Userroleasenumservice','Users','$http','$location',
+    function($scope, Authentication,Userroleasenumservice,Users, $http,$location) {
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
         $scope.userLevel = Userroleasenumservice.getValue($scope.user.roles);
 
+        console.log($scope.user);
+        if($scope.userLevel !== 4){
+            $location.path('/');
+        }
 
 
     }]);
