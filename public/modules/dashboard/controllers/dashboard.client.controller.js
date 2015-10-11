5d3b6c51aaec0b3a149f5b55e53bb2428a32eddd
Aviad Hahami <aviadhahami@gmail.com>
2015-10-11 03:33:04 +0300

    Removed unused injections

--- public/modules/dashboard/controllers/dashboard.client.controller.js
+++ public/modules/dashboard/controllers/dashboard.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('dashboard').controller('dashboardController', ['$scope', 'Authentication','Userroleasenumservice','Users','$http',
-    function($scope, Authentication,Userroleasenumservice,Users, $http) {
+angular.module('dashboard').controller('dashboardController', ['$scope', 'Authentication','Userroleasenumservice',
+    function($scope, Authentication,Userroleasenumservice) {
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
         if ($scope.user)
             $scope.userLevel = Userroleasenumservice.getValue($scope.user.roles);
