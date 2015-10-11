23226451a5f999b311337b3a6af026e3b3173240
Aviad Hahami <aviadhahami@gmail.com>
2015-10-11 17:25:37 +0300

    DANGER : revereted to regular user

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -80,7 +80,7 @@ var UserSchema = new Schema({
 	additionalProvidersData: {},
 	roles: {
 		type: String,
-		default: 'admin',
+		default: 'volunteer',
 		enum: ['user','volunteer','participant','manager', 'admin']
 	},
 	updated: {
