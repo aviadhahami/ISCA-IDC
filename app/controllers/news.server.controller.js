1c5a27e2d633d4ead6d9d629b4538c8955f1bc70
Aviad Hahami <aviadhahami@gmail.com>
2015-10-11 22:45:21 +0300

    Limited news getter to 10 posts only

--- app/controllers/news.server.controller.js
+++ app/controllers/news.server.controller.js
@@ -73,7 +73,7 @@ exports.delete = function(req, res) {
  * List of News
  */
 exports.list = function(req, res) {
-	News.find().sort('-created').populate('user', 'displayName').exec(function(err, news) {
+	News.find().limit(10).sort('-created').populate('user', 'displayName').exec(function(err, news) {
 		if (err) {
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
