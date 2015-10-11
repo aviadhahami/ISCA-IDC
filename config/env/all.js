06287f54caf3e1e2e1e7a704c982da45cef16c4c
Shay Goldman <shay@goldman.io>
2015-10-11 15:59:27 +0300

    Added angulartics lib

--- config/env/all.js
+++ config/env/all.js
@@ -31,7 +31,9 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angulartics/dist/angulartics.min.js',
+				'public/lib/angulartics-google-analytics/dist/angulartics-google-analytics.min.js'
 			]
 		},
 		svg: [
