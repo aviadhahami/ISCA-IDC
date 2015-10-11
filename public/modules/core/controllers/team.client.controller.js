b6dc22172daa12f2fb52ff4564aed83a96d31ff1
Aviad Hahami <aviadhahami@gmail.com>
2015-10-11 03:24:26 +0300

    HOTFIX: small text change

--- public/modules/core/controllers/team.client.controller.js
+++ public/modules/core/controllers/team.client.controller.js
@@ -5,7 +5,7 @@ angular.module('core').controller('TeamController', ['$scope',
         $scope.team = [
             { name: 'Roy Meirom', title: 'Program Manager', img: 'modules/core/img/team/male_avatar.png', index: 1 },
             { name: 'Adi Goldshtein', title: 'Program Coordinator', img: 'modules/core/img/team/female_avatar.png', index: 2 },
-            { name: 'Aviad Hahami', title: 'CTO', img: 'modules/core/img/team/male_avatar.png', index: 3 },
+            { name: 'Aviad Hahami', title: 'Technology Manager', img: 'modules/core/img/team/male_avatar.png', index: 3 },
             { name: 'Shay Goldman', title: 'MEAN Ninja', img: 'modules/core/img/team/ninja_avatar.png', index: 4 },
             { name: 'Ori Avraham', title: 'MEAN Ninja', img: 'modules/core/img/team/ninja_avatar.png', index: 5 },
             { name: 'Omer Karjevsky', title: 'MEAN Ninja', img: 'modules/core/img/team/ninja_avatar.png', index: 6 }
