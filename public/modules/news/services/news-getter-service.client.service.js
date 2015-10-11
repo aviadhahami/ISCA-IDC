8632b9afb6c70638014a7eae75eac24e6fa1d1d8
Aviad Hahami <aviadhahami@gmail.com>
2015-10-11 23:15:22 +0300

    Created service to return PROMISE for ten or all posts

---
+++ public/modules/news/services/news-getter-service.client.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//News service used to communicate News REST endpoints
+angular.module('news').factory('NewsGetterService', ['$http','$q',
+        function($http,$q) {
+            return {
+                getTen: function () {
+                    return $http({
+                        method : 'GET',
+                        url : 'news/limitedPosts'
+                    });
+                },
+                getAll :function(){
+                    return $http({
+                        method:'GET',
+                        url: 'news'
+                    })
+                }
+            }
+        }
+    ]
+);
