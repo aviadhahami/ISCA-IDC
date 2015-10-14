74422d5e94a874e885c9d3238b00b64953e4a3e3
Aviad Hahami <aviadhahami@gmail.com>
2015-10-14 13:29:51 +0300

    Injected exiting functions into controller, need to edit tab

--- public/modules/dashboard/controllers/users-control-panel.controller.client.js
+++ public/modules/dashboard/controllers/users-control-panel.controller.client.js
@@ -2,7 +2,27 @@
 angular.module('dashboard').controller('userControlPanelController', ['$scope', 'Authentication','Users','Userroleasenumservice',
         function($scope, Authentication, Users,Userroleasenumservice) {
 
-            console.log($scope.user)
+            var APPLICATION_TABLE = 0,
+                APPLICATION_VIEW  = 1;
+            $scope.selectedTab = APPLICATION_TABLE;
+            $scope.selectedApplication = undefined;
+
+            $scope.displayApplication = function(application) {
+                $scope.selectedTab = APPLICATION_VIEW;
+                $scope.selectedApplication = application;
+
+                var bday = new Date(application.iscaData.applicationForm.form.birthday);
+                $scope.selectedApplication.birthday = bday.getDate() + '/' + (bday.getMonth() + 1) + '/' + bday.getFullYear();
+                bday = undefined;
+                $scope.selectedApplication.languages = uniq(application.iscaData.applicationForm.form.academicInfo.languages.split(','));
+                console.log($scope.selectedApplication);
+
+            };
+
+            $scope.backToTable = function() {
+                $scope.selectedTab = APPLICATION_TABLE;
+                $scope.selectedApplication = undefined;
+            };
         }
     ]
 );
