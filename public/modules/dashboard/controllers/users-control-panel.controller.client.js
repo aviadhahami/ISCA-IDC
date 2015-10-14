a4343ff634017b34df391fad9220c0d84edac73c
Aviad Hahami <aviadhahami@gmail.com>
2015-10-14 13:20:53 +0300

    added controller for the view

---
+++ public/modules/dashboard/controllers/users-control-panel.controller.client.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('dashboard').controller('userControlPanelController', ['$scope', 'Authentication','Users','Userroleasenumservice',
+        function($scope, Authentication, Users,Userroleasenumservice) {
+
+            console.log($scope.user)
+        }
+    ]
+);
