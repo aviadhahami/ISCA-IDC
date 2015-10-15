9972a4b1c2db7cd781b5e50af7bebb8e4b462750
Aviad Hahami <aviadhahami@gmail.com>
2015-10-16 02:44:50 +0300

    Made the service a promise

--- public/modules/core/services/timetoapply.client.service.js
+++ public/modules/core/services/timetoapply.client.service.js
@@ -2,36 +2,23 @@
 
 angular.module('core').factory('Timetoapply', ['$http',
 	function($http) {
-		// Timetoapply service logic
-		// ...
 
 		// Public API
 		return {
 			setTime: function(time) {
-				$http({
+				return $http({
 					method:'post',
 					url:'api/timeToApply',
 					data:{
 						'time':time
 					}
-				}).then(function(res){
-					if (res.data.message === 'done'){
-						return true;
-					}
-				},function(err){
-					console.log(err);
-					return false;
-				});
+				})
 			},
 			getTime: function(){
-				$http({
-					method:'get',
-					url:'api/timeToApply',
-				}).then(function(res){
-					return res.data;
-				},function(err){
-					return err;
-				});
+				return $http({
+					method: 'get',
+					url: 'api/timeToApply'
+				})
 			}
 		};
 	}
