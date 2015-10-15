171dcc0ee74bee53c57f0af12357c103d1276891
Aviad Hahami <aviadhahami@gmail.com>
2015-10-16 01:36:37 +0300

    Editing controller

--- app/controllers/time-to-apply.server.controller.js
+++ app/controllers/time-to-apply.server.controller.js
@@ -6,13 +6,26 @@
 var mongoose = require('mongoose'),
     _ = require('lodash'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    pathToFolder = './mini-db/';
 
 /**
  * Create a Time to apply
  */
 exports.set = function(req, res) {
+    if (req.user.roles !== 'admin'){
+        res.status(403).send({message:'unauthorized'});
+    }
+    var fileName = 'timeToApply.json'
+    var userId = req.user._id
+    var time = req.body.time;
+    var file = require(pathToFolder+fileName);
 
+    fs.writeFile(fileName, JSON.stringify(file), function (err) {
+        if (err) return console.log(err)
+        console.log(JSON.stringify(file))
+        console.log('writing to ' + fileName)
+    });
 };
 
 /**
