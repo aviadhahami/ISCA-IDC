73b6cba093786336e463e85f554dd302e166c519
Aviad Hahami <aviadhahami@gmail.com>
2015-10-16 01:13:51 +0300

    renamed 'application' to user

--- public/modules/dashboard/controllers/users-control-panel.controller.client.js
+++ public/modules/dashboard/controllers/users-control-panel.controller.client.js
@@ -2,26 +2,31 @@
 angular.module('dashboard').controller('userControlPanelController', ['$scope', 'Authentication','Users','Userroleasenumservice',
         function($scope, Authentication, Users,Userroleasenumservice) {
 
+            // Remove duplicates from language array
+            var uniq = function (a) {
+                return a.sort().filter(function(item, pos, ary) {
+                    return !pos || item != ary[pos - 1];
+                })
+            };
             var APPLICATION_TABLE = 0,
                 APPLICATION_VIEW  = 1;
             $scope.selectedTab = APPLICATION_TABLE;
-            $scope.selectedApplication = undefined;
+            $scope.selectedUser = undefined;
 
-            $scope.displayApplication = function(application) {
+            $scope.displayApplication = function(user) {
                 $scope.selectedTab = APPLICATION_VIEW;
-                $scope.selectedApplication = application;
-
-                var bday = new Date(application.iscaData.applicationForm.form.birthday);
-                $scope.selectedApplication.birthday = bday.getDate() + '/' + (bday.getMonth() + 1) + '/' + bday.getFullYear();
+                $scope.selectedUser = user;
+                var bday = new Date(user.iscaData.applicationForm.form.birthday);
+                $scope.selectedUser.birthday = bday.getDate() + '/' + (bday.getMonth() + 1) + '/' + bday.getFullYear();
                 bday = undefined;
-                $scope.selectedApplication.languages = uniq(application.iscaData.applicationForm.form.academicInfo.languages.split(','));
-                console.log($scope.selectedApplication);
+                $scope.selectedUser.languages = uniq(user.iscaData.applicationForm.form.academicInfo.languages.split(','));
+                console.log($scope.selectedUser);
 
             };
 
             $scope.backToTable = function() {
                 $scope.selectedTab = APPLICATION_TABLE;
-                $scope.selectedApplication = undefined;
+                $scope.selectedUser = undefined;
             };
 
 
