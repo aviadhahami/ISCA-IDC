7e61a2f38473bca6c0734950704515058f846323
Aviad Hahami <aviadhahami@gmail.com>
2015-10-16 01:25:27 +0300

    Generated route and controller on server side

---
+++ app/controllers/time-to-apply.server.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    _ = require('lodash');
+
+/**
+ * Create a Time to apply
+ */
+exports.set = function(req, res) {
+
+};
+
+/**
+ * Show the current Time to apply
+ */
+exports.get = function(req, res) {
+
+};
+
+/**
+ * Update a Time to apply
+ */
+exports.update = function(req, res) {
+
+};
+
+/**
+ * Delete an Time to apply
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Time to applies
+ */
+exports.list = function(req, res) {
+
+};
--- app/routes/api.server.routes.js
+++ app/routes/api.server.routes.js
@@ -3,6 +3,10 @@
 module.exports = function(app) {
 
 	var dashboard = require('../../app/controllers/dashboard.server.controller');
+	var timeToApply = require('../../app/controllers/time-to-apply.server.controller');
 	app.route('/api/dashboard/pending').get(dashboard.pending);
 
+	// Time to apply routes
+	app.route('/api/timeToApply').get(timeToApply.get);
+	app.route('/api/timeToApply').post(timeToApply.set);
 };
