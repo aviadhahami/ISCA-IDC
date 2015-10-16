9894a17bf4d3183900153b3d49c061e9a072a590
Aviad Hahami <aviadhahami@gmail.com>
2015-10-16 03:22:18 +0300

    Editing controller and view

--- public/modules/dashboard/controllers/application-dead-line.client.controller.js
+++ public/modules/dashboard/controllers/application-dead-line.client.controller.js
@@ -4,12 +4,34 @@ angular.module('dashboard').controller('ApplicationDeadLineController', ['$scope
 	function($scope,$http,$mdDialog) {
 		// Application dead line controller logic
 		// ...
-		$scope.currentTime;
-		$http({
-			method:'get',
-			url:'api/timeToApply'
-		}).then(function(res){
-			$scope.currentTime = res.data.date;
-		})
+		var init = function(){
+			$scope.requiredDate = '';
+			$http({
+				method:'get',
+				url:'api/timeToApply'
+			}).then(function(res){
+				console.log(res.data);
+				$scope.currentTime = new Date(res.data.date);
+			});
+		};
+		init();
+
+
+		$scope.changeDeadline = function(){
+
+			$http({
+				method:'post',
+				url:'api/timeToApply',
+				data:{
+					date :$scope.requiredDate
+				}
+			}).then(function(res){
+				console.log(res.data);
+				$scope.success = 'updated date!'
+
+			},function(err){
+				$scope.error = 'didn\'t work this time...';
+			});
+		}
 	}
 ]);
