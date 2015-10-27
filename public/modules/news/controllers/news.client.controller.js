8941d2dfaeaf7f44830aa9f743acd63373199932
Aviad Hahami <aviadhahami@gmail.com>
2015-10-27 14:57:33 +0200

    Restores

---
+++ public/modules/news/controllers/news.client.controller.js
@@ -0,0 +1,70 @@
+'use strict';
+
+// News controller
+angular.module('news').controller('NewsController', ['$scope', '$stateParams', '$location', 'Authentication', 'News','Userroleasenumservice',
+	function($scope, $stateParams, $location, Authentication, News,Userroleasenumservice) {
+		$scope.authentication = Authentication;
+		$scope.user = Authentication.user;
+		$scope.userLevel = Userroleasenumservice.getValue($scope.user.roles);
+
+
+		// Create new News
+		$scope.create = function() {
+			// Create new News object
+			var news = new News ({
+				name: this.name,
+				content : this.content
+			});
+
+			// Redirect after save
+			news.$save(function(response) {
+				$location.path('news/' + response._id);
+
+				// Clear form fields
+				$scope.name = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing News
+		$scope.remove = function(news) {
+			if ( news ) {
+				news.$remove();
+
+				for (var i in $scope.news) {
+					if ($scope.news [i] === news) {
+						$scope.news.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.news.$remove(function() {
+					$location.path('news');
+				});
+			}
+		};
+
+		// Update existing News
+		$scope.update = function() {
+			var news = $scope.news;
+
+			news.$update(function() {
+				$location.path('news/' + news._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Find a list of News
+		$scope.find = function() {
+			$scope.news = News.query();
+		};
+
+		// Find existing News
+		$scope.findOne = function() {
+			$scope.news = News.get({
+				newsId: $stateParams.newsId
+			});
+		};
+	}
+]);
