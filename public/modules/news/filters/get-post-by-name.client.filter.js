54257040ca7a0506c4b73cfd7a4c0f97cbf64ea8
Aviad Hahami <aviadhahami@gmail.com>
2015-10-31 22:08:47 +0200

    Fixed check if array on filter

--- public/modules/news/filters/get-post-by-name.client.filter.js
+++ public/modules/news/filters/get-post-by-name.client.filter.js
@@ -1,11 +1,16 @@
 'use strict';
 
 angular.module('news').filter('getPostByName', [
-	function() {
-		return function(input,data) {
-			return input.filter(function(item){
-                return item.name.indexOf(data) > -1
-            })
-		};
-	}
+    function() {
+        return function(input,data) {
+            if (input) {
+
+                return input.filter(function (item) {
+                    return item.name.indexOf(data) > -1
+                })
+            }else{
+                return true;
+            }
+        }
+    }
 ]);
