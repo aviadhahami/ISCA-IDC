fa415d0ef653f846d090dfac3dae449ee980193f
aviadh <aviadhahami@gmail.com>
2015-12-06 01:57:35 +0200

    WOOHOOO resolve

--- public/modules/tasks/config/tasks.client.routes.js
+++ public/modules/tasks/config/tasks.client.routes.js
@@ -16,11 +16,10 @@ angular.module('tasks').config(['$stateProvider', '$urlRouterProvider',
             .state('taskReview', {
                 url: '/task/:taskId',
                 templateUrl: 'modules/tasks/views/task-review.client.view.html',
+                controller: 'TaskReviewController',
                 resolve: {
-                    task: ['$http','$stateParams', function ($http,$stateParams) {
-                        console.log($stateParams.taskId)
-                        $http.get('/tasks/' + $stateParams.taskId).then(function (res) {
-                            console.log(res);
+                    task: ['$http', '$stateParams', function ($http, $stateParams) {
+                        return $http.get('/tasks/' + $stateParams.taskId).then(function (res) {
                             return res.data;
                         }, function (err) {
                             return err;
--- public/modules/tasks/controllers/task-review.client.controller.js
+++ public/modules/tasks/controllers/task-review.client.controller.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('tasks').controller('TaskReviewController', ['$scope', 'Authentication', 'Userroleasenumservice',
-    function($scope, Authentication, Userroleasenumservice) {
+angular.module('tasks').controller('TaskReviewController', ['$scope', 'Authentication', 'Userroleasenumservice','task',
+    function($scope, Authentication, Userroleasenumservice,task) {
         // This provides Authentication context.
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
         $scope.userLevel = $scope.user ? Userroleasenumservice.getValue($scope.user.roles) : 0;
-        $scope.task = undefined;
 
+
+        $scope.task = task;
+        console.log(task);
         $scope.takeClicked = function() {};
         $scope.closeClicked = function() {};
 
