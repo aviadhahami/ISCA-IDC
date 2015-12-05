d5eb54c336c5cb45ce60878999edfa82aeb86d0d
aviadh <aviadhahami@gmail.com>
2015-12-06 00:30:36 +0200

    Working on view

--- public/modules/tasks/controllers/task-new.client.controller.js
+++ public/modules/tasks/controllers/task-new.client.controller.js
@@ -1,10 +1,27 @@
 'use strict';
 
-angular.module('tasks').controller('TaskReviewController', ['$scope', 'Authentication', 'Userroleasenumservice',
-    function($scope, Authentication, Userroleasenumservice) {
+angular.module('tasks').controller('TaskReviewController', ['$scope', 'Authentication', 'Userroleasenumservice', '$http',
+    function ($scope, Authentication, Userroleasenumservice, $http) {
         // This provides Authentication context.
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
         $scope.userLevel = $scope.user ? Userroleasenumservice.getValue($scope.user.roles) : 0;
 
+        console.log($scope.user);
+        // Instance for new task object
+        $scope.newTask = {
+            type: '',
+            title: '',
+            createdBy: $scope.user._id,
+            description: '',
+            content: ''
+        };
+
+        $scope.taskTypes = [];
+        $http.get('/tasks?action=types').then(function (data) {
+            $scope.taskTypes = data.data;
+        }, function (err) {
+            console.log(err);
+
+        });
     }
 ]);
