11713a07b9ad224527eaaf1df80bb21572269b7a
omerkarj <omerkarj@gmail.com>
2015-12-05 22:23:30 +0200

    tambal

---
+++ public/modules/tasks/controllers/task-review.client.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('tasks').controller('TaskNewController', ['$scope', 'Authentication', 'Userroleasenumservice',
+    function($scope, Authentication, Userroleasenumservice) {
+        // This provides Authentication context.
+        $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
+        $scope.userLevel = $scope.user ? Userroleasenumservice.getValue($scope.user.roles) : 0;
+
+    }
+]);
