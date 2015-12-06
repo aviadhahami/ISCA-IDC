2082588abc5d5a81aab731eed697292cdf36aab2
aviadh <aviadhahami@gmail.com>
2015-12-06 04:20:28 +0200

    Working on accessing edit mode

--- public/modules/tasks/controllers/task-review.client.controller.js
+++ public/modules/tasks/controllers/task-review.client.controller.js
@@ -1,27 +1,34 @@
 'use strict';
 
-angular.module('tasks').controller('TaskReviewController', ['$scope', 'Authentication', 'Userroleasenumservice','task',
-    function($scope, Authentication, Userroleasenumservice,task) {
+angular.module('tasks').controller('TaskReviewController', ['$scope', 'Authentication', 'Userroleasenumservice', 'task',
+    function ($scope, Authentication, Userroleasenumservice, task) {
         // This provides Authentication context.
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
         $scope.userLevel = $scope.user ? Userroleasenumservice.getValue($scope.user.roles) : 0;
 
 
         $scope.task = task;
+        $scope.editMode = false;
 
-
-        $scope.stringifyDate = function(date){
+        $scope.stringifyDate = function (date) {
             var time = new Date(date);
             var yyyy = time.getFullYear().toString();
-            var mm = (time.getMonth()+1).toString();
+            var mm = (time.getMonth() + 1).toString();
             var dd = time.getDate().toString();
-            return (dd[1]?dd:"0"+dd[0]) +'/' +  (mm[1]?mm:"0"+mm[0]) +'/' + yyyy;
+            return (dd[1] ? dd : "0" + dd[0]) + '/' + (mm[1] ? mm : "0" + mm[0]) + '/' + yyyy;
         };
 
-        $scope.takeTask = function() {
+        $scope.edit = function () {
+            $scope.editMode = !$scope.editMode;
+        };
+        $scope.updateRecord = function () {
+            $scope.editMode = !$scope.editMode;
 
         };
-        $scope.closeTask = function() {
+        $scope.takeTask = function () {
+
+        };
+        $scope.closeTask = function () {
 
         };
 
