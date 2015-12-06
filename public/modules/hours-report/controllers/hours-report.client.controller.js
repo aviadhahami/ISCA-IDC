3766af0b46308a7e7953603bc281c67d465e1c75
aviadh <aviadhahami@gmail.com>
2015-12-06 18:03:06 +0200

    Modifying UI

--- public/modules/hours-report/controllers/hours-report.client.controller.js
+++ public/modules/hours-report/controllers/hours-report.client.controller.js
@@ -7,13 +7,22 @@ angular.module('hoursReport').controller('HoursReportController', ['$scope', 'Au
         $scope.userLevel = $scope.user ? Userroleasenumservice.getValue($scope.user.roles) : 0;
 
         var appendTasks = function (hoursObj) {
+            //console.log(hoursObj)
             var tasks = [];
-            for (var year in hoursObj)
-                for (var month in year)
-                    for (var task in month)
-                        tasks.push(task);
+            for (var yearKey in hoursObj) {
+                for (var monthKey in hoursObj[yearKey]) {
+                    // console.log(hoursObj[yearKey][monthKey]);
+                    for (var taskKey in hoursObj[yearKey][monthKey]) {
+                        console.log(hoursObj[yearKey][monthKey][taskKey]);
+                        tasks.push(hoursObj[yearKey][monthKey][taskKey]);
+                        $scope.totalHours += hoursObj[yearKey][monthKey][taskKey]['timeTaken'];
+                    }
+                }
+            }
             return tasks;
-        }
+        };
+
+        $scope.totalHours = 0;
         if ($scope.user.iscaData && $scope.user.iscaData.hours)
             $scope.taskList = appendTasks($scope.user.iscaData.hours);
         else
