ac987f068715de7055551af43bcda402bd62ac78
omerkarj <omerkarj@gmail.com>
2015-12-06 02:36:50 +0200

    tasklist and some addition to task-review

--- public/modules/tasks/controllers/task-review.client.controller.js
+++ public/modules/tasks/controllers/task-review.client.controller.js
@@ -5,9 +5,34 @@ angular.module('tasks').controller('TaskNewController', ['$scope', 'Authenticati
         // This provides Authentication context.
         $scope.user = Authentication.hasOwnProperty('user') ? Authentication.user : null;
         $scope.userLevel = $scope.user ? Userroleasenumservice.getValue($scope.user.roles) : 0;
-        $scope.task = undefined;
 
-        $scope.takeClicked = function() {};
-        $scope.closeClicked = function() {};
+        $scope.stringifyDate = function(date){
+            var time = new Date(date);
+            var yyyy = time.getFullYear().toString();
+            var mm = (time.getMonth()+1).toString();
+            var dd = time.getDate().toString();
+            return (dd[1]?dd:"0"+dd[0]) +'/' +  (mm[1]?mm:"0"+mm[0]) +'/' + yyyy;
+        };
+        
+        $scope.takeTask = function() {
+
+        };
+        $scope.closeTask = function() {
+
+        };
+
+        $scope.task = {
+        	title: 'fuck you',
+        	type: 'Fuckbook',
+        	created: {
+        		date: Date.now(),
+        		name: 'fuckface',
+        		id: 1
+        	},
+        	status: 'new',
+        	description: 'fuck yourself in the face',
+        	content: 'fuck yourself in the facefuck yourself in the facefuck yourself in the facefuck yourself in the facefuck yourself in the facefuck yourself in the facefuck yourself in the facefuck yourself in the facefuck yourself in the face',
+
+        }
     }
 ]);
